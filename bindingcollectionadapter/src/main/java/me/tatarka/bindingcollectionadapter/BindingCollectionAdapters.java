b0961582e8fd9c1474d420b19769a7bad3599d74
Evan Tatarka <evan@tatarka.me>
2015-06-27 14:56:19 -0400

    added pattern to easily add your own bindings to a collection view

--- bindingcollectionadapter/src/main/java/me/tatarka/bindingcollectionadapter/BindingCollectionAdapters.java
+++ bindingcollectionadapter/src/main/java/me/tatarka/bindingcollectionadapter/BindingCollectionAdapters.java
@@ -1,9 +1,10 @@
 package me.tatarka.bindingcollectionadapter;
 
 import android.databinding.BindingAdapter;
+import android.support.annotation.NonNull;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
-import android.view.ViewParent;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.Collection;
@@ -15,29 +16,30 @@ public class BindingCollectionAdapters {
     @SuppressWarnings("unchecked")
     @BindingAdapter("items")
     public static <T> void setItems(RecyclerView recyclerView, Collection<T> items) {
-        BindingRecyclerViewAdapter<T> adapter = (BindingRecyclerViewAdapter<T>) recyclerView.getAdapter();
+        BindingCollectionAdapter<T> adapter = (BindingCollectionAdapter<T>) recyclerView.getAdapter();
         if (adapter != null) {
             adapter.setItems(items);
         } else {
-            recyclerView.setTag(items);
+            RecyclerViewState<T> viewState = (RecyclerViewState<T>) getViewState(recyclerView, recyclerViewStateFactory);
+            viewState.items = items;
         }
     }
 
     @SuppressWarnings("unchecked")
     @BindingAdapter("itemView")
     public static <T> void setItemView(RecyclerView recyclerView, ItemView itemView) {
-        Collection<T> items = (Collection<T>) recyclerView.getTag();
+        RecyclerViewState<T> viewState = (RecyclerViewState<T>) getViewState(recyclerView, recyclerViewStateFactory);
         BindingRecyclerViewAdapter<T> adapter = new BindingRecyclerViewAdapter<>(itemView);
-        adapter.setItems(items);
+        adapter.setItems(viewState.items);
         recyclerView.setAdapter(adapter);
     }
 
     @SuppressWarnings("unchecked")
     @BindingAdapter("itemView")
     public static <T> void setItemViewSelector(RecyclerView recyclerView, ItemViewSelector<T> selector) {
-        Collection<T> items = (Collection<T>) recyclerView.getTag();
+        RecyclerViewState<T> viewState = (RecyclerViewState<T>) getViewState(recyclerView, recyclerViewStateFactory);
         BindingRecyclerViewAdapter<T> adapter = new BindingRecyclerViewAdapter<>(selector);
-        adapter.setItems(items);
+        adapter.setItems(viewState.items);
         recyclerView.setAdapter(adapter);
     }
 
@@ -49,14 +51,12 @@ public class BindingCollectionAdapters {
     @SuppressWarnings("unchecked")
     @BindingAdapter("items")
     public static <T> void setItems(ListView listView, Collection<T> items) {
-        BindingListViewAdapter<T> adapter = (BindingListViewAdapter<T>) listView.getAdapter();
+        BindingCollectionAdapter<T> adapter = (BindingCollectionAdapter<T>) listView.getAdapter();
         if (adapter != null) {
             adapter.setItems(items);
         } else {
-            ListViewState<T> state = (ListViewState<T>) listView.getTag();
-            if (state == null) state = new ListViewState<>();
-            state.items = items;
-            listView.setTag(state);
+            ListViewState<T> viewState = (ListViewState<T>) getViewState(listView, listViewStateFactory);
+            viewState.items = items;
         }
     }
 
@@ -69,31 +69,27 @@ public class BindingCollectionAdapters {
             // We need to set the adapter again to force hasStableIds to be rechecked.
             listView.setAdapter(adapter);
         } else {
-            ListViewState<T> state = (ListViewState<T>) listView.getTag();
-            if (state == null) state = new ListViewState<>();
-            state.itemIds = itemIds;
-            listView.setTag(state);
+            ListViewState<T> viewState = (ListViewState<T>) getViewState(listView, listViewStateFactory);
+            viewState.itemIds = itemIds;
         }
     }
 
     @SuppressWarnings("unchecked")
     @BindingAdapter("itemView")
     public static <T> void setItemView(ListView listView, ItemView itemView) {
-        ListViewState<T> state = (ListViewState<T>) listView.getTag();
-        Collection<T> items = state == null ? null : state.items;
-        BindingListViewAdapter.ItemIds<T> itemIds = state == null ? null : state.itemIds;
+        ListViewState<T> viewState = (ListViewState<T>) getViewState(listView, listViewStateFactory);
         BindingListViewAdapter<T> adapter = new BindingListViewAdapter<>(itemView);
-        adapter.setItems(items);
-        adapter.setItemIds(itemIds);
+        adapter.setItems(viewState.items);
+        adapter.setItemIds(viewState.itemIds);
         listView.setAdapter(adapter);
     }
-    
+
     @SuppressWarnings("unchecked")
     @BindingAdapter("itemView")
     public static <T> void setItemViewSelector(ListView listView, ItemViewSelector<T> selector) {
-        Collection<T> items = (Collection<T>) listView.getTag();
+        ListViewState<T> viewState = (ListViewState<T>) getViewState(listView, listViewStateFactory);
         BindingListViewAdapter<T> adapter = new BindingListViewAdapter<>(selector);
-        adapter.setItems(items);
+        adapter.setItems(viewState.items);
         listView.setAdapter(adapter);
     }
 
@@ -104,13 +100,11 @@ public class BindingCollectionAdapters {
         if (adapter != null) {
             adapter.setItems(items);
         } else {
-            ViewPagerState<T> state = (ViewPagerState<T>) viewPager.getTag();
-            if (state == null) state = new ViewPagerState<>();
-            state.items = items;
-            viewPager.setTag(state);
+            ViewPagerState<T> viewState = (ViewPagerState<T>) getViewState(viewPager, viewPagerStateFactory);
+            viewState.items = items;
         }
     }
-    
+
     @SuppressWarnings("unchecked")
     @BindingAdapter("pageTitles")
     public static <T> void setPageTitles(ViewPager viewPager, BindingViewPagerAdapter.PageTitles<T> pageTitles) {
@@ -118,41 +112,88 @@ public class BindingCollectionAdapters {
         if (adapter != null) {
             adapter.setPageTitles(pageTitles);
         } else {
-            ViewPagerState<T> state = (ViewPagerState<T>) viewPager.getTag();
-            if (state == null) state = new ViewPagerState<>();
-            state.pageTitles = pageTitles;
-            viewPager.setTag(state);
+            ViewPagerState<T> viewState = (ViewPagerState<T>) getViewState(viewPager, viewPagerStateFactory);
+            viewState.pageTitles = pageTitles;
         }
     }
-        
+
     @SuppressWarnings("unchecked")
     @BindingAdapter("itemView")
     public static <T> void setItemView(ViewPager viewPager, ItemView itemView) {
-        ViewPagerState<T> state = (ViewPagerState<T>) viewPager.getTag();
-        Collection<T> items = state == null ? null : state.items;
-        BindingViewPagerAdapter.PageTitles<T> pageTitles = state == null ? null : state.pageTitles;
+        ViewPagerState<T> viewState = (ViewPagerState<T>) getViewState(viewPager, viewPagerStateFactory);
         BindingViewPagerAdapter<T> adapter = new BindingViewPagerAdapter<>(itemView);
-        adapter.setItems(items);
-        adapter.setPageTitles(pageTitles);
+        adapter.setItems(viewState.items);
+        adapter.setPageTitles(viewState.pageTitles);
         viewPager.setAdapter(adapter);
     }
 
     @SuppressWarnings("unchecked")
     @BindingAdapter("itemView")
     public static <T> void setItemViewSelector(ViewPager viewPager, ItemViewSelector<T> selector) {
-        Collection<T> items = (Collection<T>) viewPager.getTag();
+        ViewPagerState<T> viewState = (ViewPagerState<T>) getViewState(viewPager, viewPagerStateFactory);
         BindingViewPagerAdapter<T> adapter = new BindingViewPagerAdapter<>(selector);
-        adapter.setItems(items);
+        adapter.setItems(viewState.items);
+        adapter.setPageTitles(viewState.pageTitles);
         viewPager.setAdapter(adapter);
     }
-    
-    private static class ListViewState<T> {
-        Collection<T> items;
+
+    private static ViewStateFactory<RecyclerViewState> recyclerViewStateFactory = new ViewStateFactory<RecyclerViewState>() {
+        @Override
+        public RecyclerViewState newViewState() {
+            return new RecyclerViewState<>();
+        }
+    };
+
+    private static class RecyclerViewState<T> extends ViewState<T> {
+    }
+
+    private static ViewStateFactory<ListViewState> listViewStateFactory = new ViewStateFactory<ListViewState>() {
+        @Override
+        public ListViewState newViewState() {
+            return new ListViewState<>();
+        }
+    };
+
+    private static class ListViewState<T> extends ViewState<T> {
         BindingListViewAdapter.ItemIds<T> itemIds;
     }
 
-    private static class ViewPagerState<T> {
-        Collection<T> items;
+    private static ViewStateFactory<ViewPagerState> viewPagerStateFactory = new ViewStateFactory<ViewPagerState>() {
+        @Override
+        public ViewPagerState newViewState() {
+            return new ViewPagerState<>();
+        }
+    };
+
+    private static class ViewPagerState<T> extends ViewState<T> {
         BindingViewPagerAdapter.PageTitles<T> pageTitles;
+    }
+
+    /**
+     * Returns the view state for the given view, constructing it from the given factory if needed.
+     */
+    @SuppressWarnings("unchecked")
+    public static <V extends ViewState> ViewState<?> getViewState(@NonNull View view, @NonNull ViewStateFactory<V> viewStateFactory) {
+        ViewState<?> state = (ViewState<?>) view.getTag();
+        if (state == null) {
+            state = viewStateFactory.newViewState();
+            view.setTag(state);
+        }
+        return state;
+    }
+
+    /**
+     * Extra state for your adapter. This allows the state to be set before the adapter has been set
+     * on the collection view.
+     */
+    public static abstract class ViewState<T> {
+        public Collection<T> items;
+    }
+
+    /**
+     * Factory for constructing view state when needed.
+     */
+    public interface ViewStateFactory<V extends ViewState> {
+        V newViewState();
     }
 }
