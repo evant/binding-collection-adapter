eb2882aaf5d61f2af0e88842025e00550a6a2f92
喵叔catuncle <andych008@163.com>
2016-04-08 18:18:29 +0800

    checkable移入子类

--- app/src/main/java/me/tatarka/bindingcollectionadapter/sample/ItemViewModel.java
+++ app/src/main/java/me/tatarka/bindingcollectionadapter/sample/ItemViewModel.java
@@ -8,16 +8,9 @@ import android.view.View;
  * Created by evan on 6/14/15.
  */
 public abstract class ItemViewModel extends BaseObservable {
-    public final boolean checkable;
     protected String text;
 
     public ItemViewModel(String text) {
-        this.checkable = false;
-        this.text = text;
-    }
-
-    public ItemViewModel(boolean checkable, String text) {
-        this.checkable = checkable;
         this.text = text;
     }
 
@@ -29,6 +22,10 @@ public abstract class ItemViewModel extends BaseObservable {
     @Bindable
     public String getHeader() {
         return text;
+    }
+
+    public boolean isCheckable() {
+        return false;
     }
 
     @Bindable
--- app/src/main/java/me/tatarka/bindingcollectionadapter/sample/NormalItemViewModel.java
+++ app/src/main/java/me/tatarka/bindingcollectionadapter/sample/NormalItemViewModel.java
@@ -6,11 +6,17 @@ import android.view.View;
  * Created by evan on 6/14/15.
  */
 public class NormalItemViewModel extends ItemViewModel {
-
+    public final boolean checkable;
     private boolean checked;
 
     public NormalItemViewModel(int index, boolean checkable) {
-        super(checkable, index+" a item");
+        super(index+" a item");
+        this.checkable = checkable;
+    }
+
+    @Override
+    public boolean isCheckable() {
+        return checkable;
     }
 
     @Override
