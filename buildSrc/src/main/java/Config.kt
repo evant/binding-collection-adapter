a90859a579881bf12f39f4ead6fab52091249810
Dzmitry Kalianchuk <dzmitry_kalianchuk@epam.com>
2018-08-16 17:13:52 +0300

    Update tools and libs versions to actual state

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,8 @@
+object Version {
+    const val compileSdkVersion = 28
+    const val minSdkVersion14 = 14
+    const val targetSdkVersion = 28
+    const val buildToolsVersion = "28.0.1"
+    const val supportVersion = "27.1.1"
+    const val archVersion = "1.1.1"
+}
